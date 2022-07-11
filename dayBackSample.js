4604ef4ea22023f797fd83b3bdc977b0a32dcb0f
Lex Crawford <lexcraw4d@gmail.com>
2022-07-11 15:59:58 -0400

    added modal to render based on if previously selected and timeZone filter to function correctly

--- dayBackSample.js
+++ dayBackSample.js
@@ -1,67 +1,64 @@
 // Displays a timezone selector in DayBack's Settings sidebar
 // and changes timezone when you select a resource sub folder
-// with the name of the sub foldere
+// with the name of the sub folders
 // Action Type: Custom App Action - After Filter Selection
-// Prevent Default Action: No
+// Prevent Default Action: Yes
 
 // More info on custom app actions here:
 // https://docs.dayback.com/article/140-custom-app-actions
 
-
 function showModal() {
-    const title = "Time Zone Confirmation"
-    const message = "Would you like to change to xxx timezone?"
-    const cancelButtonText = "No"
-    const confirmButtonText = "Yes"
+var item = params.data.item
+// var folderName = item.folderName;
+var name = item.name
+// var status = item.status.selected;
+var resources = seedcodeCalendar.get('resources');
+  //Message displayed in modal
+  const title = "Time Zone Confirmation";
+  var message = `Would you like to change to ${name}'s timezone?`
+  const cancelButtonText = "No";
+  const confirmButtonText = "Yes";
 
-    utilities.showModal(title, message, cancelButtonText, cancelFunction, confirmButtonText, confirmFunction)
-    
+
+  //Modal display
+  utilities.showModal(
+    title,
+    message,
+    cancelButtonText,
+    null,
+    confirmButtonText,
+    confirmFunction
+  );
 }
-     function confirmFunction() {
-        debugger
-        //Is action.callbacks.confirm() needed
-        action.callbacks.confirm()
-                var stateTimezones = {
-                    "Gilliams": "America/New_York",
-                    "Les": "America/New_York",
-                    "Mom/Dad": "America/Chicago",
-                    "Chad/Beth": "America/Chicago",
-                    "Lex/Sam": "America/New_York",
-                    "John": "America/Denver",
-                    "KC": "America/Los_Angeles"
-                }
-    
-    
-                var item = params.data.item
-                var name = item.name
-                // console.log('name is:', name)
-                var status = item.status.selected
-                var resources = seedcodeCalendar.get('resources')
-                if (status) {
-                    var config = seedcodeCalendar.get('config')
-                    //selection on, deselect anything not in this folder
-                    for (var i = 0; i < resources.length; i++) {
-                        // console.log("resources[i].name is:", resources[i].name)
-                        if (resources[i].name !== name && resources[i].status && resources[i].status.selected) {
-                            resources[i].status.selected = false
-                        }
-                    }
-                    var timeZone = stateTimezones[name]
-                    config.clientTimezone = timeZone
-                    seedcodeCalendar.init('initCalendar')
-                }
-              
-            }
-        
-    
+function confirmFunction() {
+  //Custimization based on sub-folder/ sub-filter's name
+  var personalTimezones = {
+    "The Gilliam": "America/New_York",
+    "Lesley": "America/New_York",
+    "Mom/Dad": "America/Chicago",
+    "Chad/Beth": "America/Chicago",
+    "Lex/Sam": "America/New_York",
+    "John": "America/Denver",
+    "KC": "America/Los_Angeles",
+  };
 
-    function cancelFunction() {
-        // debugger
-        // cancelTimeoutCheck();
-        console.log('action is:', action)
-        action.callbacks.cancel();
-    }
+  var item = params.data.item;
+  // console.log('the item is', item)
+  var name = item.name;
+  // console.log('name is:', name)
+  var status = item.status.selected;
+  // var resources = seedcodeCalendar.get("resources");
+  if (status) {
+    var config = seedcodeCalendar.get("config");
+    var timeZone = personalTimezones[name];
+    config.clientTimezone = timeZone;
+    seedcodeCalendar.init("initCalendar");
+  }
+}
 
 
-    showModal()
- 
+let initialItem = params.data.item
+let isSelected = initialItem.status.selected
+if (isSelected === true){
+    showModal();
+}
