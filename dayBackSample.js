64819a683917cdacae48803ff554bca0ef9df38e
Lex Crawford <lexcraw4d@gmail.com>
2022-07-12 16:02:34 -0400

    refactoring mvp

--- dayBackSample.js
+++ dayBackSample.js
@@ -1,65 +1,70 @@
 // Displays a timezone selector in DayBack's Settings sidebar
-// and changes timezone when you select a resource sub folder
-// with the name of the sub folders
+// and changes timezone based on tags associated with a given resource
+
 // Action Type: Custom App Action - After Filter Selection
 // Prevent Default Action: No
 // More info on custom app actions here:
 // https://docs.dayback.com/article/140-custom-app-actions
 
+//here we declare initial variables
+let config = seedcodeCalendar.get("config");
 let initialItem = params.data.item;
 let isSelected = initialItem.status.selected;
+let data = params.data.item.tags;
+const title = "* Time Zone Confirmation *";
+const cancelButtonText = "No";
+const confirmButtonText = "Yes";
 
-// console.log('tagArr', tagArr)
+//timezones currently used (you can customize these to meet your personal/business/teams needs)
 
-if (isSelected === true) {
-  showModal();
+let personalTimezones = {
+  eastern: "America/New_York",
+  est:"America/New_York",
+  central: "America/Chicago",
+  central: "America/Chicago",
+  mountain: "America/Denver",
+  mdt: "America/Denver",
+ 'los angeles': "America/Los_Angeles",
+  la: "America/Los_Angeles",
+  pdt: "America/Los_Angeles",
+ 'west coast' : "America/Los_Angeles",
+  pacific: "Pacific/Honolulu",
+  pst:"Pacific/Honolulu"
+};
+
+if (data) {
+  for (let i = 0; i < data.length; i++) {
+    let tagName = data[i].name;
+    //convert to lowercase and reassign tagName 
+    tagName = tagName.toLowerCase()
+    //future enhancements could be to add switch case e.g.
+    getTimeZone(tagName);
+  }
 }
-//Personalized DayBack Calendar based on filtering through family/co-workers and their given timezones
-function showModal() {
-  //Message displayed in modal
-  const title = "Time Zone Confirmation";
-  //Template literal added for customization in presenting the selected filters name
-  var message = `Would you like to change to the selected timezone?`;
-  const cancelButtonText = "No";
-  const confirmButtonText = "Yes";
-  //Modal display method and callbacks
-  //if user selects no then null is used as the cancelFunction
+
+
+function getTimeZone(tagName) {
+    //here we pass in tag name to the modal for template literal usage and also to our confirm function based on user response to modal
+    if (isSelected === true) {
+      let timeZone = personalTimezones[tagName];
+      confirmFunction(timeZone)
+      showModal(timeZone);
+    }
+}
+
+//Personalized DayBack Calendar based on filtering by tag names 
+function showModal(timeZone) {
+  let message = `Would you like to change to ${timeZone} timezone?`;
   utilities.showModal(
     title,
     message,
     cancelButtonText,
-    null,
+    confirmFunction,
     confirmButtonText,
-    confirmFunction
+    null
   );
 }
-function confirmFunction() {
-  //Custimization based on sub-folder/ sub-filter's name
-  //Optional to set to your own needs
-
-  var data = params.data.item.tags;
-  if (data) {
-    for (let i = 0; i < data.length; i++) {
-      let tagName = data[i].name;
-      //convert to lowercase
-      //add switch case statement if eastern || est : Eastern
-      getTimeZone(tagName);
-    }
-    function getTimeZone(tagName) {
-      var personalTimezones = {
-        Eastern : "America/New_York",
-        Central: "America/Chicago",
-        Mountain: "America/Denver",
-        Los_Angeles: "America/Los_Angeles",
-        Pacific : "Pacific/Honolulu"
-      };
-      //here get calandar based on selected person's timezone
-      if (tagName) {
-        var config = seedcodeCalendar.get("config");
-        var timeZone = personalTimezones[tagName];
-        config.clientTimezone = timeZone;
-        seedcodeCalendar.init("initCalendar");
-      }
-    }
+function confirmFunction(timeZone) {
+    config.clientTimezone = timeZone
+     seedcodeCalendar.init("initCalendar");
   }
-}
