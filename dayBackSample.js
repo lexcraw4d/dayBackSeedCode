e158147f72301d89749b1a131b59411ba8f9c13b
Lex Crawford <lexcraw4d@gmail.com>
2022-07-12 01:14:32 -0400

    mvp

--- dayBackSample.js
+++ dayBackSample.js
@@ -6,18 +6,23 @@
 // More info on custom app actions here:
 // https://docs.dayback.com/article/140-custom-app-actions
 
+let initialItem = params.data.item;
+let isSelected = initialItem.status.selected;
 
+// console.log('tagArr', tagArr)
+
+if (isSelected === true) {
+  showModal();
+}
 //Personalized DayBack Calendar based on filtering through family/co-workers and their given timezones
 function showModal() {
-var item = params.data.item
-var name = item.name
   //Message displayed in modal
   const title = "Time Zone Confirmation";
   //Template literal added for customization in presenting the selected filters name
-  var message = `Would you like to change to ${name}'s timezone?`
+  var message = `Would you like to change to the selected timezone?`;
   const cancelButtonText = "No";
   const confirmButtonText = "Yes";
-  //Modal display method and callbacks 
+  //Modal display method and callbacks
   //if user selects no then null is used as the cancelFunction
   utilities.showModal(
     title,
@@ -31,32 +36,30 @@ var name = item.name
 function confirmFunction() {
   //Custimization based on sub-folder/ sub-filter's name
   //Optional to set to your own needs
-  var personalTimezones = {
-    "The Gilliam": "America/New_York",
-    "Lesley": "America/New_York",
-    "Mom/Dad": "America/Chicago",
-    "Chad/Beth": "America/Chicago",
-    "Lex/Sam": "America/New_York",
-    "John": "America/Denver",
-    "KC": "America/Los_Angeles",
-  };
 
-  var item = params.data.item;
-  var name = item.name;
-  var status = item.status.selected;
-  //here get calandar based on selected person's timezone
-  if (status) {
-    var config = seedcodeCalendar.get("config");
-    var timeZone = personalTimezones[name];
-    config.clientTimezone = timeZone;
-    seedcodeCalendar.init("initCalendar");
+  var data = params.data.item.tags;
+  if (data) {
+    for (let i = 0; i < data.length; i++) {
+      let tagName = data[i].name;
+      //convert to lowercase
+      //add switch case statement if eastern || est : Eastern
+      getTimeZone(tagName);
+    }
+    function getTimeZone(tagName) {
+      var personalTimezones = {
+        Eastern : "America/New_York",
+        Central: "America/Chicago",
+        Mountain: "America/Denver",
+        Los_Angeles: "America/Los_Angeles",
+        Pacific : "Pacific/Honolulu"
+      };
+      //here get calandar based on selected person's timezone
+      if (tagName) {
+        var config = seedcodeCalendar.get("config");
+        var timeZone = personalTimezones[tagName];
+        config.clientTimezone = timeZone;
+        seedcodeCalendar.init("initCalendar");
+      }
+    }
   }
-}
-
-//conditional based on if the filtered item is truly selected then show modal
-// if not then do nothing
-let initialItem = params.data.item
-let isSelected = initialItem.status.selected
-if (isSelected === true){
-    showModal();
 }
